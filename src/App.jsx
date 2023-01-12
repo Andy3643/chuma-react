d8c7ae457c2e1f4927fa9f2dfac4de92148c8d89
@Andy <joshua.mulonga@student.moringaschool.com>
2023-01-13 00:05:38 +0300

    ft-design responsive navbar

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Plans from './pages/plans/Plans'
+import Trainers from './pages/trainers/Trainers'
+import Notfound from './pages/notfound/Notfound'
+import Navbar from './components/Navbar'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Contact/>
+        <Gallery/>
+        <Plans/>
+        <Trainers/>
+        <Notfound/>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/trainers/Trainers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Trainers.css'
+const Trainers = () => {
+  return (
+    <div>Trainers</div>
+  )
+}
+
+export default Trainers
